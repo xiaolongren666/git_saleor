582c692eab921a494fc27c002ba6e13f3533293b
xiaolongren666 <1315516121@qq.com>
2018-10-04 16:57:42 +0800

    clean out things

--- saleor/Procfile
+++
@@ -1,2 +0,0 @@
-release: python manage.py migrate --no-input
-web: uwsgi saleor/wsgi/uwsgi.ini
--- saleor/saleor/graphql/shipping/resolvers.py
+++
@@ -1,5 +0,0 @@
-from ...shipping import models
-
-
-def resolve_shipping_methods(info):
-    return models.ShippingMethod.objects.all()
